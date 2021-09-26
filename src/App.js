c3e86b91ac0ce69229cdae97da570302f901f9ce
wldns0622 <wldns12378@gmail.com>
2021-09-27 00:14:02 +0900

    feat: Mock서버 테스팅

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import axios from 'axios';
+const MOCK_SERVER = 'https://d0f6acf8-864b-4048-909a-2b302f66e04a.mock.pstmn.io';
 
 function App() {
-  console.log
+  let result = axios.get(`${MOCK_SERVER}/locations`);
+  console.log(result);
+  return <h1>Hello World</h1>;
 }
 
 export default App;
